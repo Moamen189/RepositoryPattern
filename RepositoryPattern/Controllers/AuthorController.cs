3a28b031befa393af8a69aae0d47d752d4b687bb
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-02-28 15:30:01 +0200

    Update

---
+++ RepositoryPattern/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using BusniessLogicLayer.IRepository;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IGenericRepository<Author> authorRepository;
+
+        public AuthorController(IGenericRepository<Author> authorRepository)
+        {
+            this.authorRepository = authorRepository;
+        }
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            return Ok(authorRepository.GetById(id));
+        }
+    }
+}
