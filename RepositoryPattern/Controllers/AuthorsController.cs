e14355fe27f404ce3f671b3522071d07061f39a5
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-02-28 15:25:50 +0200

    Update

---
+++ RepositoryPattern/Controllers/AuthorsController.cs
@@ -0,0 +1,15 @@
+﻿using BusniessLogicLayer.IRepository;
+using DataAccessLayer.Models;
+
+namespace RepositoryPattern.Controllers
+{
+    public class AuthorsController
+    {
+        private readonly IGenericRepository<Author> genericRepository;
+
+        public AuthorsController(IGenericRepository<Author> genericRepository)
+        {
+            this.genericRepository = genericRepository;
+        }
+    }
+}
